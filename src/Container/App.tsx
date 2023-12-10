8cdf2719ca607c4eb4b29a67f8393f9bd2bdf531
MehemmedBulut <mehemmedbulut@gmail.com>
2023-12-10 19:49:59 +0400

    initial commit message

---
+++ src/Container/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Footer, Header } from '../Components/Layout/Index';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      Main component
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
