99eae9a91ab6ffda89d41daaf0f3a62173277c97
MehemmedBulut <mehemmedbulut@gmail.com>
2023-12-17 14:02:33 +0400

    cart summary

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Footer, Header } from '../Components/Layout/Index';
-import { Home, MenuItemDetails, NotFound } from '../Pages';
+import { Home, MenuItemDetails, NotFound, ShoppingCart } from '../Pages';
 import { Routes,Route } from 'react-router-dom';
-
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setShoppingCart } from '../Storage/Redux/shoppingCartSlice';
+import { useGetShoppingCartQuery } from '../Apis/ShoppingCartApi';
 
 function App() {
+  const dispatch = useDispatch();
 
+  const {data,isLoading} = useGetShoppingCartQuery(
+    "df71d35a-4793-4d50-906c-0ef5b69d20c8"
+    );
+
+    useEffect(()=>{
+      if(!isLoading){
+        console.log(data.result);
+        dispatch(setShoppingCart(data.result?.carItems))
+      }
+    }, [data]);
 
   return (
     <div>
@@ -13,7 +27,10 @@ function App() {
       <div className='pb-3'>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
-          <Route path="/meuItemDetails/:menuItemId" element={<MenuItemDetails/>}></Route>
+          <Route path="/meuItemDetails/:menuItemId" 
+          element={<MenuItemDetails/>}>
+          </Route>
+          <Route path="/ShoppingCart" element={<ShoppingCart/>}></Route>
           <Route path="*" element={<NotFound/>}></Route>
         </Routes>
       </div>
